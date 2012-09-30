dc77b959b72b48848c34486df078f30a5c551c04
Cody Sommer <codisimus@gmail.com>
2012-09-30 17:24:52 -0400

    implemented logger

--- src/com/codisimus/plugins/boobietrap/BoobieTrap.java
+++ src/com/codisimus/plugins/boobietrap/BoobieTrap.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.util.HashMap;
 import java.util.Properties;
+import java.util.logging.Logger;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -22,56 +23,58 @@ import org.bukkit.scheduler.BukkitScheduler;
  * @author Codisimus
  */
 public class BoobieTrap extends JavaPlugin implements Listener {
-    static BukkitScheduler scheduler;
-    static HashMap<Location, Integer> explosives = new HashMap<Location, Integer>();
+    private static BukkitScheduler scheduler;
+    private static Logger logger;
+    private static HashMap<Location, Integer> explosives = new HashMap<Location, Integer>();
     Properties p;
-    int timer = 5;
+    int delay = 5;
 
-    @Override
-    public void onDisable () {
-    }
-    
     @Override
     public void onEnable () {
         //Register Events
         this.getServer().getPluginManager().registerEvents(this, this);
         scheduler = this.getServer().getScheduler();
+        logger = this.getLogger();
         loadSettings();
-        
-        System.out.println("BoobieTrap "+this.getDescription().getVersion()+" is enabled!");
+
+        Properties version = new Properties();
+        try {
+            version.load(this.getResource("version.properties"));
+        } catch (Exception ex) {
+        }
+        logger.info("BoobieTrap " + this.getDescription().getVersion()
+                + " (Build " + version.getProperty("Build") + ") is enabled!");
     }
-    
+
     /**
      * Loads settings from the config.properties file
-     * 
      */
     public void loadSettings() {
         FileInputStream fis = null;
         try {
             //Copy the file from the jar if it is missing
             File file = this.getDataFolder();
-            if (!file.isDirectory())
+            if (!file.isDirectory()) {
                 file.mkdir();
-            file = new File(file.getPath()+"/config.properties");
-            if (!file.exists())
+            }
+            file = new File(file.getPath() + "/config.properties");
+            if (!file.exists()) {
                 this.saveResource("config.properties", true);
-            
+            }
+
             //Load config file
             p = new Properties();
             fis = new FileInputStream(file);
             p.load(fis);
-            
-            timer = Integer.parseInt(loadValue("Timer"));
-        }
-        catch (Exception missingProp) {
-            System.err.println("Failed to load BoobieTrap "+this.getDescription().getVersion());
+
+            delay = Integer.parseInt(loadValue("Delay"));
+        } catch (Exception missingProp) {
+            logger.severe("Failed to load BoobieTrap " + this.getDescription().getVersion());
             missingProp.printStackTrace();
-        }
-        finally {
+        } finally {
             try {
                 fis.close();
-            }
-            catch (Exception e) {
+            } catch (Exception e) {
             }
         }
     }
@@ -83,58 +86,67 @@ public class BoobieTrap extends JavaPlugin implements Listener {
      * @return The String value of the loaded key
      */
     private String loadValue(String key) {
-        //Print an error if the key is not found
         if (!p.containsKey(key)) {
-            System.err.println("[BoobieTrap] Missing value for "+key+" in config file");
-            System.err.println("[BoobieTrap] Please regenerate config file");
+            logger.severe("Missing value for " + key + " in config file");
+            logger.severe("Please regenerate config file");
         }
-        
+
         return p.getProperty(key);
     }
-    
+
     @EventHandler (ignoreCancelled=true, priority = EventPriority.MONITOR)
     public void onChestOpen(PlayerInteractEvent event) {
-        if (event.getAction() != Action.RIGHT_CLICK_BLOCK)
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) {
             return;
-        
+        }
+
         final Block block = event.getClickedBlock();
-        if (block.getType() != Material.CHEST)
+        if (block.getType() != Material.CHEST) {
             return;
-        
+        }
+
         final Inventory inv = ((Chest)block.getState()).getInventory();
         if (isExplosive(inv)) {
             final Location location = block.getLocation();
-            if (explosives.containsKey(location))
+            if (explosives.containsKey(location)) {
                 return;
-            
+            }
+
             explosives.put(location, scheduler.scheduleAsyncDelayedTask(this, new Runnable() {
                 @Override
                 public void run() {
                     inv.clear();
+                    block.setTypeId(0);
                     block.getWorld().createExplosion(location.add(0, 2, 0), 6F, true);
                 }
-            }, timer * 20L));
+            }, delay * 20L));
         }
     }
-    
+
     @EventHandler (ignoreCancelled=true, priority = EventPriority.MONITOR)
     public void onBlockBreak(BlockBreakEvent event) {
         Location location = event.getBlock().getLocation();
-        if (!explosives.containsKey(location))
+        if (!explosives.containsKey(location)) {
             return;
-        
+        }
+
         scheduler.cancelTask(explosives.get(location));
         explosives.remove(location);
     }
-    
+
     public boolean isExplosive(Inventory inv) {
-        if (inv.firstEmpty() < 9)
+        if (inv.firstEmpty() < 9) {
             return false;
-        
-        return inv.getItem(0).getType() == Material.TNT && inv.getItem(1).getType() == Material.REDSTONE &&
-                inv.getItem(2).getType() == Material.TNT && inv.getItem(3).getType() == Material.REDSTONE &&
-                inv.getItem(4).getType() == Material.LEVER &&
-                inv.getItem(5).getType() == Material.REDSTONE && inv.getItem(6).getType() == Material.TNT &&
-                inv.getItem(7).getType() == Material.REDSTONE && inv.getItem(8).getType() == Material.TNT;
+        }
+
+        return inv.getItem(0).getType() == Material.TNT
+                && inv.getItem(1).getType() == Material.REDSTONE
+                && inv.getItem(2).getType() == Material.TNT
+                && inv.getItem(3).getType() == Material.REDSTONE
+                && inv.getItem(4).getType() == Material.LEVER
+                && inv.getItem(5).getType() == Material.REDSTONE
+                && inv.getItem(6).getType() == Material.TNT
+                && inv.getItem(7).getType() == Material.REDSTONE
+                && inv.getItem(8).getType() == Material.TNT;
     }
 }
